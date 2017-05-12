277d81de69d1331bb71211dc4fe2c4352ac509ee
kartwal <kamilarturwalas@gmail.com>
2017-05-12 18:11:49 +0200

    improved login func, added simple list download func

--- platforms/browser/www/js/formValidation.js
+++ platforms/browser/www/js/formValidation.js
@@ -1,3 +1,6 @@
+var userCredentialsData = {};
+
+
 function validateLogin() {
   $("#loginForm").validate({
     rules: {
@@ -21,56 +24,23 @@ function validateLogin() {
 
     submitHandler: function(form) {
 
-      $(document).ajaxStart(function() {
-          $.mobile.loading('show');
-      });
-      // $.mobile.pageContainer.pagecontainer('change', '#listPage', {reverse: false, changeHash: true, transition: 'slide'});
+      // $(document).ajaxStart(function() {
+      //     $.mobile.loading('show');
+      // });
+
       var values = {};
       $.each($('#loginForm').serializeArray(), function(i, field) {
           values[field.name] = field.value;
       });
 
-      // navigator.notification.activityStart("Please Wait", "Its loading.....");
-      var postTo = 'http://kartwal.ayz.pl/EventGuide_API/v1/index.php/login';
-
       console.log("Starting...");
-    $.post(postTo,({email: values["email"], password: values["password"]}),
-    function(data) {
 
-      console.log( data.error );
-        if(data != "") {
-          if (data.error === true)
-          {
-            console.log( data.error );
-            $(document).ajaxStop(function() {
-                $.mobile.loading('hide');
-            });
-          }
-          else {
-            console.log("ok");
-            $(document).ajaxStop(function() {
-                $.mobile.loading('hide');
-            });
-            $.mobile.pageContainer.pagecontainer('change', '#listPage', {reverse: false, changeHash: true, transition: 'slide'});
-          }
-        } else {
-            console.log("błąd połączenia");
-        }
-        },'json');
-        return false;
+      loginInto(values["email"], values["password"]);
+
+      return false;
 
     }
   });
-}
-
-function alertCallback()
-{
-
-}
-
-function onConfirm(buttonIndex) {
-    alert('You selected button ' + buttonIndex);
-    var options = { dimBackground: true };
 }
 
 function validateRegister() {
@@ -146,6 +116,84 @@ function validateRegister() {
 function closeRegisterPopup()
 {
   $('#popupRegister').popup('close');
+}
+
+function loginInto(userEmail, userPassword)
+{
+  console.log(userEmail, userPassword);
+  $.ajax({
+            type: "POST",
+            url: "http://kartwal.ayz.pl/EventGuide_API/v1/index.php/login",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: jQuery.param({email: userEmail, password: userPassword}),
+            success: function (response) {
+              if (response["error"] == true) {
+                console.log(response["message"]);
+              }
+              else {
+
+                console.log(response);
+                userCredentialsData = response;
+                $.mobile.pageContainer.pagecontainer('change', '#listPage', {reverse: false, changeHash: true, transition: 'slide'});
+              }
+            },
+            error: function (errormessage) {
+                console.log(errormessage);
+            }
+        });
+
+}
+$(document).on('pageshow', "#listPage",function(){
+
+  console.log('pageinit');
+
+
+});
+
+function downloadEventsList()
+{
+  $.ajax({
+            type: "GET",
+            url: "http://kartwal.ayz.pl/EventGuide_API/v1/index.php/getEventsList",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            headers: { 'Authorization': userLoginData["apiKey"] },
+            success: function (msg) {
+               console.log(msg);
+            },
+            error: function (errormessage) {
+                console.log(errormessage);
+            }
+        });
+
+}
+
+
+
+function downloadEventDetails(detailsID)
+{
+  $.ajax({
+            type: "GET",
+            url: "http://kartwal.ayz.pl/EventGuide_API/v1/index.php/event/" + detailsID,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            headers: { 'Authorization': '7194581c02ab6087e7da5881be984fe0' },
+            success: function (msg) {
+               console.log(msg);
+            },
+            error: function (errormessage) {
+                console.log("elo");
+            }
+        });
+
+}
+
+
+function onConfirm(buttonIndex) {
+    alert('You selected button ' + buttonIndex);
+    var options = { dimBackground: true };
 }
 
 function scan()
