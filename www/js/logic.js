ea3c993468c126fbf78915691e672de654f8d283
adaa0704 <ada.galka0704@gmail.com>
2017-05-17 17:08:02 +0200

    added sharing plugin

--- www/js/logic.js
+++ www/js/logic.js
@@ -495,19 +495,30 @@ function sendInvs(){
 
     }
 
-    cordova.plugins.email.isAvailable(
+    // cordova.plugins.email.isAvailable(
+    //
+    //     function (isAvailable) {
+    //         alert('Before proceeding check your mail settings ...') ;
+    //     }
+    // );
 
-        function (isAvailable) {
-            alert('Before proceeding check your mail settings ...') ;
-        }
-    );
+    // cordova.plugins.email.open({
+    // to:      'contact@kartwal.pl',
+    // subject: 'EventGuide invitation',
+    // body:    '<h1>You have invitation from EventGuide APP.</h1><p>Scan QR Code with Event Guide App<p><img class="eventQRCode" id="QRcode" src=' + response["QR Code"] + '>',
+    // isHtml:  true
+    // });
 
-    cordova.plugins.email.open({
-    to:      'contact@kartwal.pl',
-    subject: 'EventGuide invitation',
-    body:    '<h1>You have invitation from EventGuide APP.</h1><p>Scan QR Code with Event Guide App<p><img class="eventQRCode" id="QRcode" src=' + response["QR Code"] + '>',
-    isHtml:  true
-});
+    window.plugins.socialsharing.shareViaEmail(
+  'Message', // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
+  'Subject',
+  ['to@person1.com', 'to@person2.com'], // TO: must be null or an array
+  ['cc@person1.com'], // CC: must be null or an array
+  null, // BCC: must be null or an array
+  ['https://www.google.nl/images/srpr/logo4w.png','www/localimage.png'], // FILES: can be null, a string, or an array
+  onSuccess, // called when sharing worked, but also when the user cancelled sharing via email. On iOS, the callbacks' boolean result parameter is true when sharing worked, false if cancelled. On Android, this parameter is always true so it can't be used). See section "Notes about the successCallback" below.
+  onError // called when sh*t hits the fan
+);
 }
 
 document.addEventListener('deviceready', function () {
