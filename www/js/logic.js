4030d8b9e17ab39373974d51b8dfdd05284e57d0
kartwal <kamilarturwalas@gmail.com>
2017-05-17 17:54:36 +0200

    checking katzer plugin

--- www/js/logic.js
+++ www/js/logic.js
@@ -516,5 +516,24 @@ function sendInvs(){
 }
 
 document.addEventListener('deviceready', function () {
-    alert("Loaded ok");
+    cordova.plugins.email.isAvailable(
+        function (isAvailable) {
+            alert("is email mobile available? " + (isAvailable ? "Yes" : "No"));
+            if(isAvailable){
+             window.plugin.email.open({
+                 to:      'test@test.com',
+                 subject: 'Greetings',
+                 body:    'How are you? Nice greetings from Leipzig'
+             }, callback, scope);
+           }
+        }
+    );
 }, false);
+
+function callback(){
+    console.log("callback function");
+}
+
+function scope(){
+    console.log("scope function");
+}
